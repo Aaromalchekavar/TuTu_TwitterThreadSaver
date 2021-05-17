d2fcd7bf62a6697f7e66bbbf518097d09811f17e
Aaromalchekavar <binilbijuchengankal@gmail.com>
2021-05-17 19:44:36 +0530

    Initial Commit

---
+++ config.py
@@ -0,0 +1,23 @@
+import tweepy
+import logging
+import devcredentials
+
+
+logger = logging.getLogger()
+
+def create_api():
+   
+
+    auth = tweepy.OAuthHandler(devcredentials.API_Key, devcredentials.API_Secret_Key)
+    auth.set_access_token(devcredentials.Acess_Token, devcredentials.Acess_Token_Secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True, 
+        wait_on_rate_limit_notify=True)
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error creating API", exc_info=True)
+        raise e
+    logger.info("API created")
+    return api
+
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from config import create_api
+
+
