35496454d9301a062442f8a31f99ffdac2b55d25
Aaromalchekavar <binilbijuchengankal@gmail.com>
2021-05-21 01:44:49 +0530

    Bot Initial

--- config.py
+++ config.py
@@ -5,13 +5,15 @@ import devcredentials
 
 logger = logging.getLogger()
 
-def create_api():
-   
 
-    auth = tweepy.OAuthHandler(devcredentials.API_Key, devcredentials.API_Secret_Key)
-    auth.set_access_token(devcredentials.Acess_Token, devcredentials.Acess_Token_Secret)
-    api = tweepy.API(auth, wait_on_rate_limit=True, 
-        wait_on_rate_limit_notify=True)
+def create_api():
+
+    auth = tweepy.OAuthHandler(
+        devcredentials.API_Key, devcredentials.API_Secret_Key)
+    auth.set_access_token(devcredentials.Acess_Token,
+                          devcredentials.Acess_Token_Secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True,
+                     wait_on_rate_limit_notify=True)
     try:
         api.verify_credentials()
     except Exception as e:
@@ -21,3 +23,9 @@ def create_api():
     return api
 
 
+def twitter_auth():
+    auth = tweepy.OAuthHandler(
+        devcredentials.API_Key, devcredentials.API_Secret_Key)
+    auth.set_access_token(devcredentials.Acess_Token,
+                          devcredentials.Acess_Token_Secret)
+    return auth
--- main.py
+++ main.py
@@ -1,3 +1,50 @@
-from config import create_api
+import tweepy
+from config import twitter_auth
+
+idFile = open('./files/inReplyStatusId.txt', 'a')
+file = open('./files/inReplyStatusId.txt')
+idFileData = file.readlines()
+
+auth = twitter_auth()
+api = tweepy.API(auth)
+
+user = api.get_user("ChekavarAaromal")
+mentions = api.mentions_timeline()
+
+
+for mention in mentions:
+    user_ = mention.user
+    recipent_id = user_.id_str
+    print("Recipent ID : " + recipent_id)
+    inReply_to_status_id = mention.in_reply_to_status_id_str
+    print(inReply_to_status_id)
+    match_value = str(inReply_to_status_id)
+    if inReply_to_status_id != None:
+        status = api.get_status(inReply_to_status_id).text
+        boolean = match_value not in idFileData
+    else:
+        status = "No Text in Tweet. Plese Reply to any valid tweet"
+        boolean = False
+    if boolean:
+        print(match_value + " not found thus sending tweets to dm")
+        print(status)
+        event = {
+        "event": {
+            "type": "message_create",
+            "message_create": {
+            "target": {
+                "recipient_id": recipent_id
+            },
+            "message_data": {
+                "text": status
+            }
+            }
+        }
+        }
+        api.send_direct_message_new(event)
+        idFile.writelines("\n"+match_value)
+
+
+
 
 
