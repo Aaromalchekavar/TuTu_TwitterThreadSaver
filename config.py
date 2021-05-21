ddafb4c4401b0bd91539ba1805e487cd78dd77e1
Aaromalchekavar <binilbijuchengankal@gmail.com>
2021-05-21 20:50:01 +0530

    virtualenv variables added

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import tweepy
 import logging
 import devcredentials
+import os
 
 
 logger = logging.getLogger()
@@ -8,10 +9,15 @@ logger = logging.getLogger()
 
 def create_api():
 
+    consumer_key = os.getenv("CONSUMER_KEY")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
     auth = tweepy.OAuthHandler(
-        devcredentials.API_Key, devcredentials.API_Secret_Key)
-    auth.set_access_token(devcredentials.Acess_Token,
-                          devcredentials.Acess_Token_Secret)
+        consumer_key, consumer_secret)
+    auth.set_access_token(access_token,
+                          access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
     try:
